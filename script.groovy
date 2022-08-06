8362e5c778cd9621f001e60295f2dfb7709de223
jcmeunier77code <jcmeunier77code@gmail.com>
2022-08-06 10:07:51 +0200

    feat: ok

--- script.groovy
+++ script.groovy
@@ -9,7 +9,7 @@ def buildImage() {
         sh 'docker build -t jcmeunier77/bootcamp-java-maven-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push jcmeunier77/bootcamp-java-maven-app:jma-2.0'
-    }
+}
 
 def deployApp() {
     echo "Deploying the application...YES!!!"
